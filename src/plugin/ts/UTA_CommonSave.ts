ad011d54edc1544f41d6d44127e69a28e65283c6
t-akatsuki <t-akatsuki@utakata-no-yume.net>
2020-08-22 02:08:46 +0900

    [add] UTA_CommonSaveのRPGツクールMZ版。とりあえず動作する版。

---
+++ src/plugin/ts/UTA_CommonSave.ts
@@ -0,0 +1,522 @@
+//=============================================================================
+// UTA_CommonSave.js
+//=============================================================================
+/*:
+ * @target MZ
+ * @plugindesc hogehoge
+ * @author t-akatsuki
+ * @url https://www.utakata-no-yume.net
+ *
+ * @help てすと
+ */
+
+/*:ja
+ * @target MZ
+ * @plugindesc hogehoge
+ *
+ * @author 赤月 智平
+ * @url https://www.utakata-no-yume.net
+ *
+ * @param saveFileName
+ * @text 共有セーブデータファイル名
+ * @desc 共有セーブデータを記録するセーブデータファイル名の定義です。
+ * 拡張子は自動設定される為含めません。
+ * @default uta_common
+ * @type string
+ *
+ * @param targetSwitches
+ * @text 共有対象スイッチ番号
+ * @desc セーブデータ間で共有するスイッチ番号の定義です。
+ * 「-」で範囲指定が可能です。
+ * @default
+ * @type string[]
+ *
+ * @param targetVariables
+ * @text 共有対象変数番号
+ * @desc セーブデータ間で共有する変数番号の定義です。
+ * 「-」で範囲指定が可能です。
+ * @default
+ * @type string[]
+ *
+ * @param applyOnLoad
+ * @text ロード時の共有セーブ自動適用
+ * @desc ロード時に共有セーブデータを自動適用を行うか。
+ * true: 自動適用する, false: 自動適用しない
+ * @default true
+ * @type boolean
+ *
+ * @param applyOnSave
+ * @text セーブ時の共有セーブ自動保存
+ * @desc セーブ時に共有セーブデータの自動保存を行うか。
+ * true: 自動保存する, false: 自動保存しない
+ * @default true
+ * @type boolean
+ *
+ * @param applyOnAutoSave
+ * @text オートセーブ時の共有セーブ自動保存
+ * @desc オートセーブ時に共有セーブの自動保存を行うか。
+ * true: 自動保存する, false: 自動保存しない
+ * @default false
+ * @type boolean
+ *
+ * @param applyOnGameover
+ * @text ゲームオーバー時の共有セーブ自動保存
+ * @desc ゲームオーバー時に共有セーブデータの自動保存を行うか。
+ * true: 自動保存する, false: 自動保存しない
+ * @default false
+ * @type boolean
+ *
+ * @command load
+ * @text 共有セーブデータのロード
+ * @desc 共有セーブデータからスイッチ・変数を読み込み反映させます。
+ * 任意のタイミングで共有セーブデータをロードする際に使用します。
+ *
+ * @command save
+ * @text 共有セーブデータのセーブ
+ * @desc 共有セーブデータに対象のスイッチ・変数の状態を記録します。
+ * 任意のタイミングで共有セーブデータをセーブする際に使用します。
+ *
+ * @command remove
+ * @text 共有セーブデータの削除
+ * @desc 共有セーブデータファイルを削除します。
+ * 共有セーブデータをリセットしたい場合に使用します。
+ *
+ * @command check
+ * @text 共有対象スイッチ/変数の確認
+ * @desc 共有対象のスイッチ/変数番号をコンソールに表示します。
+ * 動作確認用のプラグインコマンドです。
+ *
+ * @help
+ *
+ * ■プラグインコマンド
+ *
+ * ■プラグインの情報
+ *   バージョン : 0.1.0
+ *   最終更新日 : 2020.08.21
+ *   制作者     : 赤月 智平(t-akatsuki)
+ *   Webサイト  : https://www.utakata-no-yume.net
+ *   GitHub     : https://github.com/t-akatsuki/RMMZ_UTA_CommonSave
+ *   Twitter    : @T_Akatsuki
+ *   ライセンス : MIT License
+ *
+ * ■更新履歴
+ *   0.1.0 (2020/08/21)
+ *     ほげほげ
+ */
+/**
+ * @namespace utakata
+ */
+namespace utakata {
+    "use strict";
+
+    interface CommonSavePluginParameters {
+        saveFileName: string;
+        targetSwitches: string;
+        targetVariables: string;
+        applyOnLoad: string;
+        applyOnSave: string;
+        applyOnAutoSave: string;
+        applyOnGameover: string;
+    }
+
+    interface CommonSaveData {
+        gameSwitches: { [idx: number]: boolean };
+        gameVariables: { [idx: number]: number };
+    }
+
+    /**
+     * @static
+     * @class CommonSave
+     * @classdesc 共有セーブ関連の処理を扱う静的クラス
+     */
+    export class CommonSave {
+        /**
+         * プラグイン名称定義
+         * @type {string}
+         */
+        public static readonly PLUGIN_NAME = "UTA_CommonSave";
+        /**
+         * 共有対象のスイッチ番号定義
+         * @type {number[]}
+         */
+        private static targetSwitches: number[] = [];
+        /**
+         * 共有対象の変数番号定義
+         * @type {number[]}
+         */
+        private static targetVariables: number[] = [];
+        /**
+         * プラグインパラメーターの参照
+         * @type {object}
+         */
+        private static parameters: CommonSavePluginParameters;
+
+        constructor() {
+            throw new Error("CommonSave is static class.");
+        }
+
+        /**
+         * 初期化処理
+         * @static
+         */
+        public static initialize(): void {
+            this.targetSwitches = [];
+            this.targetVariables = [];
+            this.parameters = <CommonSavePluginParameters>PluginManager.parameters(this.PLUGIN_NAME);
+
+            // プラグインパラメーターに指定された値を読み込む
+            var targetSwitchesList: string[] = JsonEx.parse(this.parameters.targetSwitches);
+            var targetVariablesList: string[] = JsonEx.parse(this.parameters.targetVariables);
+            this.loadTargetSwitchesNumber(targetSwitchesList);
+            this.loadTargetVariablesNumber(targetVariablesList);
+        }
+
+        /**
+         * 文字列配列から対象番号配列をロードする。
+         * 重複する番号が含まれていた場合はuniqueな状態になる。
+         * @param {string[]} targetList 読み込み対象番号文字配列。
+         * @return {number[]} 対象番号配列。
+         */
+        private static loadTargetCoreNumber(targetList: string[]): number[] {
+            var ret: number[] = [];
+            for (const targetStr of targetList) {
+                const targetList = this.parseTargetNumber(targetStr);
+                Array.prototype.push.apply(ret, targetList);
+            }
+            // unique
+            ret = ret.filter((x: number, i: number, self: number[]) => {
+                return self.indexOf(x) === i;
+            });
+            return ret;
+        }
+
+        /**
+         * 共有対象スイッチ番号を読み込む。
+         * @static
+         * @param {string[]} targetSwitches 読み込み対象のスイッチ番号文字配列。
+         * プラグインパラメーターに設定された配列を渡す。
+         */
+        private static loadTargetSwitchesNumber(targetSwitches: string[]): void {
+            this.targetSwitches = this.loadTargetCoreNumber(targetSwitches);
+        }
+
+        /**
+         * 共有対象変数番号を読み込む。
+         * @static
+         * @param {string[]} targetVariables 読み込み対象の変数番号文字配列。
+         * プラグインパラメーター似設定された配列を渡す。
+         */
+        private static loadTargetVariablesNumber(targetVariables: string[]): void {
+            this.targetVariables = this.loadTargetCoreNumber(targetVariables);
+        }
+
+        /**
+         * 引数で渡した数字文字列をparseして対象番号のリストを得る。
+         * 「-]を利用して範囲指定された番号の解釈も行う。
+         * @static
+         * @param {string} targetStr parse対象の文字列。
+         * @return {number[]} 文字列から得た対象番号のリスト。
+         */
+        private static parseTargetNumber(targetStr: string): number[] {
+            var ret: number[] = [];
+            const singlePattern = /^[0-9]+$/;
+            const regionPattern = /^[0-9]+-[0-9]+$/;
+
+            try {
+                targetStr = targetStr.trim();
+
+                if (targetStr.match(singlePattern)) {
+                    // single number
+                    // ex. 10
+                    const num = parseInt(targetStr, 10);
+                    if (num !== num) {
+                        throw new Error(this.PLUGIN_NAME + ": parse error: NaN returned. (" + targetStr + ")");
+                    }
+                    ret.push(num);
+                } else if (targetStr.match(regionPattern)) {
+                    // region number
+                    // ex. 10-15
+                    const numStrs = targetStr.split("-");
+                    const sNum = parseInt(numStrs[0], 10);
+                    const eNum = parseInt(numStrs[1], 10);
+                    if (sNum !== sNum || eNum !== eNum) {
+                        throw new Error(this.PLUGIN_NAME + ": parse error: NaN returned. (" + targetStr + ")");
+                    }
+                    if (sNum >= eNum) {
+                        throw new Error(this.PLUGIN_NAME + ": parse error: invaliad format. (" + targetStr + ")");
+                    }
+                    for (let num = sNum; num <= eNum; num++) {
+                        ret.push(num);
+                    }
+                } else {
+                    throw new Error(this.PLUGIN_NAME + ": parse error: invaliad format. (" + targetStr + ")");
+                }
+            } catch (e) {
+                console.error("CommonSave.parseTargetNumber: Invalid format string has comming. (" + targetStr + ")");
+                console.error(e.message);
+                console.error(e.stack);
+                throw e;
+            }
+            return ret;
+        }
+
+        /**
+         * 現在の$gameSwitchesのデータから、共有セーブ対象としているものを連想配列で取得する。
+         * @static
+         * @return {object} 共有セーブ対象$gameSwitchesの連想配列。
+         */
+        private static makeTargetGameSwitchesJson(): { [idx: number]: boolean } {
+            var ret: { [idx: number]: boolean } = {};
+            for (const idx of this.targetSwitches) {
+                // $gameSwitchesは触った事が無いと要素が作られないので範囲チェックできない
+                if (idx < 1) {
+                    console.warn("ComonSave.getTargetSwitchesJson: target switch index is out of range. (" + idx + ")");
+                    continue;
+                }
+                ret[idx] = $gameSwitches.value(idx);
+            }
+            return ret;
+        }
+
+        /**
+         * 現在の$gameVariablesのデータから、共有セーブの対象としているものを連想配列で取得する。
+         * @static
+         * @return {object} 共有セーブ対象$gameVariablesの連想配列。
+         */
+        private static makeTargetGameVariablesJson(): { [idx: number]: number } {
+            var ret: { [idx: number]: number } = {};
+            for (const idx of this.targetVariables) {
+                // $gameVariablesは触った事が無いと要素が作られないので範囲チェックできない
+                if (idx < 1) {
+                    console.warn("ComonSave.getTargetGameVariablesJson: target variable index is out of range. (" + idx + ")");
+                    continue;
+                }
+                ret[idx] = $gameVariables.value(idx);
+            }
+            return ret;
+        }
+
+        /**
+         * 共有セーブするデータを作成する。
+         * @static
+         * @return {object} 共有セーブするデータの連想配列。
+         */
+        private static makeSaveContents(): CommonSaveData {
+            const contents: CommonSaveData = {
+                "gameSwitches": this.makeTargetGameSwitchesJson(),
+                "gameVariables": this.makeTargetGameVariablesJson()
+            };
+            return contents;
+        }
+
+        /**
+         * プラグインパラメーターで設定された共有セーブデータファイル名を取得する。
+         * 拡張子は含めない。
+         * @static
+         * @return {string} 共有セーブデータファイル名の文字列。
+         */
+        private static makeSaveName(): string {
+            return this.parameters.saveFileName;
+        }
+
+        /**
+         * 共有セーブデータが存在するか。
+         * @static
+         * @return {boolean} 共有セーブデータが存在する場合trueを返す。
+         */
+        private static exists(): boolean {
+            const saveName = this.makeSaveName();
+            return StorageManager.exists(saveName);
+        }
+
+        /**
+         * 共有セーブオブジェクトから$gameSwitchesにデータをロードする。
+         * @static
+         * @param {object} contents ロードした共有セーブデータオブジェクト
+         */
+        private static loadCommonSaveSwitches(contents: CommonSaveData): void {
+            if (!("gameSwitches" in contents)) {
+                return;
+            }
+            for (const key in contents.gameSwitches) {
+                const idx = parseInt(key, 10);
+                const value = contents.gameSwitches[key];
+                $gameSwitches.setValue(idx, value);
+            }
+            return;
+        }
+
+        /**
+         * 共有セーブオブジェクトから$gameVariablesにデータをロードする。
+         * @static
+         * @param {object} contents ロードした共有セーブデータオブジェクト
+         */
+        private static loadCommonSaveVariables(contents: CommonSaveData): void {
+            if (!("gameVariables" in contents)) {
+                return;
+            }
+            for (const key in contents.gameVariables) {
+                const idx = parseInt(key, 10);
+                const value = contents.gameVariables[key];
+                $gameVariables.setValue(idx, value);
+            }
+            return;
+        }
+
+        /**
+         * 共有セーブデータのロード処理。
+         * local版, web版共通。
+         * @static
+         * @return {Promise<number>}
+         */
+        public static load(): Promise<number> {
+            // 共有セーブデータが存在しない場合は何もしない
+            // 途中から導入した場合を考慮してエラーにしない事
+            if (!this.exists()) {
+                return new Promise((resolve) => {
+                    console.info("ComonSave.load: common save is not existed.");
+                    resolve(0);
+                });
+            }
+            const saveName = this.makeSaveName();
+            return StorageManager.loadObject(saveName).then((contents: CommonSaveData) => {
+                this.loadCommonSaveSwitches(contents);
+                this.loadCommonSaveVariables(contents);
+                console.log("CommonSave.load: completed to load common save.");
+                return 0;
+            });
+        }
+
+        /**
+         * 共有セーブデータのセーブ処理。
+         * local版, web版共通。
+         * @static
+         * @return {Promise<number>}
+         */
+        public static save(): Promise<number> {
+            const contents = this.makeSaveContents();
+            const saveName = this.makeSaveName();
+            return StorageManager.saveObject(saveName, contents).then(() => {
+                console.log("CommonSave.save: completed to save common save.");
+                return 0;
+            });
+        }
+
+        /**
+         * 共有セーブデータを削除する。
+         * @static
+         */
+        public static remove(): void {
+            const saveName = this.makeSaveName();
+            StorageManager.remove(saveName);
+            console.log("CommonSave.remove: remove common save data. (" + saveName + ")");
+        }
+
+        /**
+         * 現在共有対象としているスイッチ/変数番号をコンソールに表示する。
+         * デバッグ用機能。
+         * @static
+         */
+        public static check(): void {
+            console.info("CommonSave.check: current target switches number.");
+            console.info(this.targetSwitches.join(","));
+            console.info("CommonSave.check: current target variables number.");
+            console.info(this.targetVariables.join(","));
+        }
+
+        /**
+         * セーブデータロード時に共有セーブデータをロードするか。
+         * @static
+         * @return {boolean}
+         */
+        public static isApplyOnLoad(): boolean {
+            return this.parameters.applyOnLoad === "true";
+        }
+
+        /**
+         * セーブデータセーブ時に共有セーブデータをセーブするか。
+         * @static
+         * @return {boolean}
+         */
+        public static isApplyOnSave(): boolean {
+            return this.parameters.applyOnSave === "true";
+        }
+
+        /**
+         * オートセーブ時に共有セーブデータをセーブするか。
+         * @static
+         * @return {boolean}
+         */
+        public static isApplyOnAutoSave(): boolean {
+            return this.parameters.applyOnAutoSave === "true";
+        }
+
+        /**
+         * ゲームオーバー時に共有セーブデータをセーブするか。
+         * @static
+         * @return {boolean}
+         */
+        public static isApplyOnGameover(): boolean {
+            return this.parameters.applyOnGameover === "true";
+        }
+    }
+    CommonSave.initialize();
+
+    /**
+     * register plugin command
+     */
+    PluginManager.registerCommand(CommonSave.PLUGIN_NAME, "load", () => {
+        CommonSave.load();
+    });
+    PluginManager.registerCommand(CommonSave.PLUGIN_NAME, "save", () => {
+        CommonSave.save();
+    });
+    PluginManager.registerCommand(CommonSave.PLUGIN_NAME, "remove", () => {
+        CommonSave.remove();
+    });
+    PluginManager.registerCommand(CommonSave.PLUGIN_NAME, "check", () => {
+        CommonSave.check();
+    });
+
+    /**
+     * DataManager
+     */
+    const _DataManager_saveGame = DataManager.saveGame;
+    DataManager.saveGame = function (savefileId: number): Promise<number> {
+        return _DataManager_saveGame.call(this, savefileId).then((ret: number) => {
+            // savefileId = 0 is auto save
+            if (savefileId === 0) {
+                if (CommonSave.isApplyOnAutoSave()) {
+                    return CommonSave.save();
+                }
+                return ret;
+            }
+
+            if (CommonSave.isApplyOnSave()) {
+                return CommonSave.save();
+            }
+            return ret;
+        });
+    };
+
+    const _DataManager_loadGame = DataManager.loadGame;
+    DataManager.loadGame = function (savefileId: number): Promise<number> {
+        return _DataManager_loadGame.call(this, savefileId).then((ret: number) => {
+            if (CommonSave.isApplyOnLoad()) {
+                return CommonSave.load();
+            }
+            return ret;
+        });
+    };
+
+    /**
+     * Scene_Gameover
+     */
+    const _Scene_Gameover_prototype_start = Scene_Gameover.prototype.start;
+    Scene_Gameover.prototype.start = function (): void {
+        if (CommonSave.isApplyOnGameover()) {
+            CommonSave.save();
+        }
+        _Scene_Gameover_prototype_start.call(this);
+    };
+}
