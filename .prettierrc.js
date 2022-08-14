ed0865dae5c80086475e54fd75e992b3c158eb91
t-akatsuki <t-akatsuki@utakata-no-yume.net>
2022-08-15 00:19:20 +0900

    [add] prettier用設定ファイルを追加。eslintと連携させる。

---
+++ .prettierrc.js
@@ -0,0 +1,25 @@
+/**
+ * Prettier設定
+ * 設定は以下を参照
+ * https://prettier.io/docs/en/options.html
+ */
+ module.exports = {
+    // 1行あたりに最大文字数
+    "printWidth": 120,
+    // indentのスペース数
+    "tabWidth": 4,
+    // indentにタブを利用するか
+    "useTabs": false,
+    // 行末にセミコロンを付けるか
+    "semi": true,
+    // ダブルクォートの代わりにシングルクォートを使うか
+    "singleQuote": false,
+    // 末尾のカンマのルール
+    "trailingComma": "es5",
+    // ブラケット内にスペースを必須とするか
+    "bracketSpacing": false,
+    // アロー関数における括弧を必須とするか
+    "arrowParens": "always",
+    // 改行コードの指定
+    "endOfLine": "lf"
+}
