464e9f378ff50912f5eebeb44ac2f56272549f9b
t-akatsuki <t-akatsuki@utakata-no-yume.net>
2025-03-28 23:09:16 +0900

    add: バージョンを扱うVersionクラスを追加。
    - バージョンの判定に利用する。

--- src/js/UTA_CommonSaveMZ.js
+++ src/js/UTA_CommonSaveMZ.js
@@ -177,6 +177,161 @@ utakata.UTA_CommonSaveMZ = (function() {
         return Logger;
     })();
 
+    /**
+     * @class Version
+     * @classdesc セマンティックバージョニング2.0.0形式のバージョンを扱うクラス。
+     */
+    const Version = (function() {
+        const _logger = Logger.getLogger("Version");
+
+        /**
+         * @constructor
+         * @param {number} major メジャーバージョン値。
+         * @param {number} minor マイナーバージョン値。
+         * @param {number} patch パッチバージョン値。
+         */
+        function Version(major, minor, patch) {
+            /**
+             * メジャーバージョン。
+             * @type {number}
+             */
+            this.major = major;
+
+            /**
+             * マイナーバージョン。
+             * @type {number}
+             */
+            this.minor = minor;
+
+            /**
+             * パッチパージョン。
+             * @type {number}
+             */
+            this.patch = patch;
+        }
+
+        /**
+         * バージョン値の区切り文字の定義。
+         * @readonly
+         * @type {string}
+         */
+        Object.defineProperty(Version, "SEPARATOR", {
+            value: ".",
+            writable: false
+        });
+
+        /**
+         * バージョンが比較対象と同じであるかを判定する。
+         * @param {Version} other 比較対象バージョン。
+         * @return {boolean} 比較対象と同じバージョンの場合はtrueを返す。
+         */
+        Version.prototype.isEqual = function(other) {
+            let ret = true;
+            for (const target of ["major", "minor", "patch"]) {
+                ret &= this[target] === other[target];
+            }
+            return ret;
+        };
+
+        /**
+         * バージョンが比較対象より古いかを判定する。
+         * @param {Version} other 比較対象バージョン。
+         * @return {boolean} 比較対象より古いバージョンの場合はtrueを返す。
+         */
+        Version.prototype.isOlderThan = function(other) {
+            let ret = false;
+            for (const target of ["major", "minor", "patch"]) {
+                if (this[target] < other[target]) {
+                    ret = true;
+                    break;
+                }
+            }
+            return ret;
+        };
+
+        /**
+         * バージョンが比較対象より新しいかを判定する。
+         * @param {Version} other 比較対象バージョン。
+         * @return {boolean} 比較対象より新しい場合はtrueを返す。
+         */
+        Version.prototype.isLaterThan = function(other) {
+            let ret = false;
+            for (const target of ["major", "minor", "patch"]) {
+                if (this[target] > other[target]) {
+                    ret = true;
+                    break;
+                }
+            }
+            return ret;
+        };
+
+        /**
+         * バージョンが比較対象以前のものかを判定する。
+         * @param {Version} other 比較対象バージョン。
+         * @return {boolean} 比較対象以前の場合はtrueを返す。
+         */
+        Version.prototype.isOlderThanEqual = function(other) {
+            return this.isOlderThan(other) || this.isEqual(other);
+        };
+
+        /**
+         * バージョンが比較対象以後のものかを判定する。
+         * @param {Version} other 比較対象バージョン。
+         * @return {boolean} 比較対象以後の場合はtrueを返す。
+         */
+        Version.prototype.isLaterThanEqual = function(other) {
+            return this.isLaterThan(other) || this.isEqual(other);
+        };
+
+        /**
+         * バージョン情報を連想配列として取得する。
+         * @return {Object.<string, number>} バージョン情報の連想配列。
+         */
+        Version.prototype.getVersionDict = function() {
+            let ret = {};
+            const target = ["major", "minor", "patch"];
+            for (const prop of target) {
+                ret[prop] = this[target];
+            }
+            return ret;
+        };
+
+        /**
+         * バージョン文字列に変換する。
+         * @return {string} バージョン文字列。
+         */
+        Version.prototype.toString = function() {
+            const versionStr = Array.prototype.join.call([
+                this.major, this.minor, this.patch
+            ], Version.SEPARATOR);
+            return versionStr;
+        };
+
+        /**
+         * バージョン文字列からバージョンインスタンスを得る。
+         * @param {string} versionStr バージョン文字列。
+         * @return {Version} バージョンインスタンス。
+         */
+        Version.fromString = function(versionStr) {
+            let version = null;
+            try {
+                const versionStrList = versionStr.split(Version.SEPARATOR);
+                if (versionStrList.length !== 3) {
+                    throw new TypeError(`Version string invalid format.`);
+                }
+                version = this(...versionStrList);
+            } catch (e) {
+                _logger(Logger.ERROR, `fromString: Version string parse error. (versionStr = ${versionStr})`);
+                _logger(Logger.ERROR, e);
+                throw UTA_CommonSaveError(`Parse error: Version string parse error.`);
+            }
+
+            return version;
+        };
+
+        return Version;
+    })();
+
     // 名前空間越しにアクセス可能なプロパティの定義
     const exports = {
         VERSION: VERSION
